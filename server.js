7f2b22848aca2fbc35f78bdc953bdc049ea3b4fc
sushil-011bq <sushil@bytequests.com>
2026-09-25 17:35:32 +0530

    feature private messaging

--- server.js
+++ server.js
@@ -26,37 +26,79 @@ app.get("/", async (request, reply) => {
   return reply.sendFile("index.html");
 });
 
+
+const users = new Map();
+
+function sendUsers() {
+  const userList = Array.from(users.entries()).map(([socketId, user]) => ({
+    socketId,
+    username: user.username,
+  }));
+
+  io.emit("users", userList);
+}
+
 io.on("connection", (socket) => {
   console.log("Client connected:", socket.id);
 
   // 1. Specific client
-  socket.on("private_test", (message) => {
-    console.log(message,'data')
-    socket.emit("private_response", {
-      from: "server",
-      message: message + ' private response',
+//   socket.on("private_test", (message) => {
+//     console.log(message,'data')
+//     socket.emit("private_response", {
+//       from: "server",
+//       message: message + ' private response',
+//     });
+//   });
+
+//   // 2. Everyone except sender
+//   socket.on("broadcast_test", (data) => {
+//     socket.broadcast.emit("broadcast_response", {
+//       from: socket.id,
+//       message: data.message,
+//     });
+//   });
+
+//   // 3. Everyone including sender
+//   socket.on("global_test", (data) => {
+//     io.emit("global_response", {
+//       from: socket.id,
+//       message: data.message,
+//     });
+//   });
+
+//   socket.on("disconnect", () => {
+//     console.log("Client disconnected:", socket.id);
+//   });
+
+
+console.log("Client connected:", socket.id);
+
+socket.on("join", ({ username }) => {
+  users.set(socket.id, {
+    username,
+  });
+
+  console.log("User joined:", username, "Socket:", socket.id);
+  console.log(users)
+  sendUsers();
+});
+
+
+socket.on("private_message", ({ to, message }) => {
+    io.to(to).emit("private_message", {
+      senderId: socket.id,
+      message,
+      timestamp: Date.now(),
     });
   });
 
-  // 2. Everyone except sender
-  socket.on("broadcast_test", (data) => {
-    socket.broadcast.emit("broadcast_response", {
-      from: socket.id,
-      message: data.message,
-    });
-  });
+socket.on("disconnect", () => {
+  users.delete(socket.id);
+  sendUsers();
+  console.log("Client disconnected:", socket.id);
+});
 
-  // 3. Everyone including sender
-  socket.on("global_test", (data) => {
-    io.emit("global_response", {
-      from: socket.id,
-      message: data.message,
-    });
-  });
 
-  socket.on("disconnect", () => {
-    console.log("Client disconnected:", socket.id);
-  });
 });
 
 app.listen(
